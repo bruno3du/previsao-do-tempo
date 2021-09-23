d1d8e82514d332b8c87490cf47bc03858a7c6226
bruno3du <bruno.3duardo@hotmail.com>
2021-09-23 17:15:24 -0300

    Corrigindo acessibilidade

--- src/App.js
+++ src/App.js
@@ -8,9 +8,7 @@ function App() {
 	return (
 		<section>
 			<div className='container mt-5 d-flex flex-column'>
-				<strong>
-					<h1 className='text-light ms-3 mb-5 display-1'>Previsão do Tempo</h1>
-				</strong>
+				<h1 className='text-light ms-3 mb-5 display-1'>Previsão do Tempo</h1>
 				<Search />
 				<hr className='bg-light' />
 				<h2 className='text-light ms-3 mb-5 display-1'>Capitais</h2>
